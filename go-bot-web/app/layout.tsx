c4186288a93ad5553f4c4041fce2df099078a66c
tjgersho <tjgersho@gmail.com>
2025-12-09 09:48:25 -0700

    page view

--- go-bot-web/app/layout.tsx
+++ go-bot-web/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,11 +29,21 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+         {/* X (Twitter) Pixel */}
+         <Script
+          id="twitter-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+              },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+              a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+              twq('config','qvlpx');
+              twq('event', 'tw-qvlpx-qvlvt', {});
+            `,
+          }}
+        />
       </body>
-      <script type="text/javascript">
-        twq('event', 'tw-qvlpx-qvlvt', {
-        });
-      </script>
     </html>
   );
 }
